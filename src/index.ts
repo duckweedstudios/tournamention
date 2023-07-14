6e58ede45e96c31a02ab167158087c684db5ab65
coreybriscoe <coreybriscoe@gmail.com>
2023-07-14 14:10:17 -0400

    feat(backend): Add database connection at startup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { Client } from 'discord.js';
+import mongoose from 'mongoose';
 import example from './example.js';
 import example2 from './example2.js';
+
+// Database connection
+mongoose.connect(process.env.DB_URI as string)
+.then(() => {
+    console.log('Database connection established');
+}).catch((err) => {
+    console.log(`Error connecting to database: ${err}}`);
+});
 
 const client = new Client({
     intents: [],
