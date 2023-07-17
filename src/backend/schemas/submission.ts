2c8096e7350ebe14e2008b08d39999d135fefced
coreybriscoe <coreybriscoe@gmail.com>
2023-07-17 16:29:59 -0400

    feat(backend): Add SubmissionStatus enum

--- src/backend/schemas/submission.ts
+++ src/backend/schemas/submission.ts
@@ -4,6 +4,12 @@ import { Judge } from './judge';
 import { Challenge } from './challenge';
 import { Contestant } from './contestant';
 
+export enum SubmissionStatus {
+    Pending = 'Pending',
+    Accepted = 'Accepted',
+    Rejected = 'Rejected',
+}
+
 export class ReviewNote {
     _id!: ObjectId;
 
@@ -37,7 +43,7 @@ export const SubmissionModel = getModelForClass(Submission);
 
 SubmissionModel.schema.virtual('status').get((s: Submission) => {
     if (s.reviewNotes.length === 0) {
-        return 'Pending';
+        return SubmissionStatus.Pending;
     }
     return s.reviewNotes.reduce(
         (prev: ReviewNote, curr: ReviewNote) => {
