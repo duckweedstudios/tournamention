8b5f062d7d3377201cc9629863a1dc47f7994473
coreybriscoe <coreybriscoe@gmail.com>
2023-07-18 15:18:04 -0400

    refactor: Move instance method definition
    See https://typegoose.github.io/typegoose/docs/guides/quick-start-guide/#instance-methods
    
    However, I wasn't able to name a parameter `this` as that documentation suggests. TS seems to know what's going on without it, so hopefully that works.

--- src/backend/schemas/guildsettings.ts
+++ src/backend/schemas/guildsettings.ts
@@ -1,29 +1,27 @@
 import { ObjectId } from 'mongodb';
 import { Document } from 'mongoose';
-import { prop, getModelForClass } from '@typegoose/typegoose';
+import { prop, getModelForClass, /*DocumentType*/ } from '@typegoose/typegoose';
 import { TournamentModel, Tournament } from './tournament';
 
 export class GuildSettings {
     @prop({ required: true, unique: true })
     public guildID!: string;
+
+    public async getCurrentTournament(/*this: DocumentType<GuildSettings>*/) {
+        const guildTournaments = (await TournamentModel.find({ guildID: this.guildID }));
+        const activeTournament = guildTournaments
+            .map((t: Document<ObjectId, unknown, Tournament>) => {
+                return t.toObject();
+            })
+            .filter((t: Tournament) => {
+                return t.active;
+            })
+            .reduce((prev: Tournament, curr: Tournament) => {
+                // TODO: this may not work
+                return !prev.name && prev._id.getTimestamp().getTime() > curr._id.getTimestamp().getTime() ? prev : curr;
+            }, new Tournament());
+        return activeTournament ? activeTournament : null;
+    }
 }
 
 export const GuildSettingsModel = getModelForClass(GuildSettings);
-
-// Oddly, TypeScript suggests `this` is undefined in the method, hence the parameter
-GuildSettingsModel.schema.methods.getCurrentTournament = async (thisGuildID: string) => {
-    if (!this) return null;
-    const guildTournaments = (await TournamentModel.find({ guildID: thisGuildID }));
-    const activeTournament = guildTournaments
-        .map((t: Document<ObjectId, unknown, Tournament>) => {
-            return t.toObject();
-        })
-        .filter((t: Tournament) => {
-            return t.active;
-        })
-        .reduce((prev: Tournament, curr: Tournament) => {
-            // TODO: this may not work
-            return !prev.name && prev._id.getTimestamp().getTime() > curr._id.getTimestamp().getTime() ? prev : curr;
-        }, new Tournament());
-    return activeTournament ? activeTournament : null;
-};
