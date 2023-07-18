ce2f28582865811fcf222f14b8ac3f83152b1a71
coreybriscoe <coreybriscoe@gmail.com>
2023-07-18 15:57:33 -0400

    fix: Fix Contestant uniqueness constraint
    Only one Contestant should exist per pair of userID and guildID

--- src/backend/schemas/contestant.ts
+++ src/backend/schemas/contestant.ts
@@ -1,5 +1,6 @@
-import { prop, getModelForClass } from '@typegoose/typegoose';
+import { prop, index, getModelForClass } from '@typegoose/typegoose';
 
+@index({ userID: 1, guildID: 1 }, { unique: true })
 export class Contestant {
     @prop({ required: true })
     public userID!: string;
@@ -9,4 +10,3 @@ export class Contestant {
 }
 
 export const ContestantModel = getModelForClass(Contestant);
-ContestantModel.schema.index({ userID: 1, guildID: 1 }, { unique: true });
