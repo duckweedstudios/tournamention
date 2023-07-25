d8add2059af59cc17afdee72567e2526332ffe4d
coreybriscoe <coreybriscoe@gmail.com>
2023-07-25 14:58:05 -0400

    feat: Add sample slash command
    This command simply echoes back the string option provided (or undefined) for now. New commands can use this file as a template.

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -5,9 +5,11 @@ import { CustomCommand } from '../types/customCommand.js';
 const InfoCommand = new CustomCommand(
     new SlashCommandBuilder()
         .setName('info')
-        .setDescription('Display Tournamention bot information.'),
+        .setDescription('Display Tournamention bot information.')
+        .addStringOption(option => option.setName('info').setDescription('The information to display.').setRequired(false)) as SlashCommandBuilder,
     async (interaction: CommandInteraction) => {
-        interaction.reply('Hello world!');
+        console.log('Hello world!');
+        interaction.reply({ content: `Hello world! ${interaction.options.get('info')?.value}`, ephemeral: true });
     }
 );
 
