58133b6d382c6cb23986d1a7568ddf01b3872919
coreybriscoe <coreybriscoe@gmail.com>
2023-09-28 21:00:40 -0400

    feat: Add slash command architecture logic

---
+++ src/types/defaultSlashCommandDescriptions.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { OutcomeStatus, Outcome, SlashCommandDescribedOutcome, OutcomeWithMonoBody, OutcomeWithDuoBody } from './outcome.js';
+
+export const defaultSlashCommandDescriptions = new Map<OutcomeStatus, (o: Outcome<string>) => SlashCommandDescribedOutcome>([
+    [OutcomeStatus.SUCCESS, (_: Outcome<string>) => {
+        return {
+            userMessage: '✅ Success!',
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.SUCCESS_MONO, (_: Outcome<string>) => {
+        return {
+            userMessage: `✅ Success! (default response, 1 data point omitted)`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.SUCCESS_DUO, (_: Outcome<string>) => {
+        return {
+            userMessage: `✅ Success! (default response, 2 data points omitted)`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.SUCCESS_NO_CHANGE, (_: Outcome<string>) => {
+        return {
+            userMessage: `✅ Success! However, certain operations made no changes.`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.FAIL, (_: Outcome<string>) => {
+        return {
+            userMessage: '❌ This command failed.',
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.FAIL_VALIDATION, (o: Outcome<string>) => {
+        return {
+            userMessage: `❌ This command failed due to entered data ${(o as OutcomeWithMonoBody<string>).body.data}`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.FAIL_DNE_MONO, (o: Outcome<string>) => {
+        return {
+            userMessage: `❌ This command failed because the data ${(o as OutcomeWithMonoBody<string>).body.data} could not be found.`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.FAIL_DNE_DUO, (o: Outcome<string>) => {
+        return {
+            userMessage: `❌ This command failed because ${(o as OutcomeWithDuoBody<string>).body.data1} and ${(o as OutcomeWithDuoBody<string>).body.data2} do not exist together.`,
+            ephemeral: true,
+        };
+    }],
+    [OutcomeStatus.FAIL_UNKNOWN, (_: Outcome<string>) => {
+        return {
+            userMessage: '❌ This command failed for an unknown reason.',
+            ephemeral: true,
+        };
+    }],
+]);
---
+++ src/types/outcome.ts
@@ -0,0 +1,60 @@
+import { Constraint } from '../commands/slashcommands/architecture/validation.js';
+
+export type SlashCommandDescribedOutcome = {
+    userMessage: string,
+    ephemeral: boolean,
+};
+
+export enum OutcomeStatus {
+    SUCCESS = 'SUCCESS', // generic success
+    SUCCESS_NO_CHANGE = 'SUCCESS_NO_CHANGE', // trivial success with no change
+    SUCCESS_MONO = 'SUCCESS_MONO', // success with one datapoint
+    SUCCESS_DUO = 'SUCCESS_DUO', // success with two datapoints
+    FAIL = 'FAIL', // generic failure
+    FAIL_VALIDATION = 'FAIL_VALIDATION', // premature failure due to validation error
+    FAIL_DNE_MONO = 'FAIL_DNE_MONO', // nonexistence failure with one datapoint, indicating that the target does not exist
+    FAIL_DNE_DUO = 'FAIL_DNE_DUO', // nonexistencec failure with two datapoints, indicating that the joint target does not exist
+    FAIL_UNKNOWN = 'FAIL_UNKNOWN', // failure with unknown reason
+}
+
+// Shorthand for the enforced empty object type.
+type EmptyObject = Record<string, never>;
+
+export type OutcomeWithEmptyBody = {
+    status: OutcomeStatus.SUCCESS | OutcomeStatus.FAIL | OutcomeStatus.FAIL_UNKNOWN,
+    body: EmptyObject,
+};
+
+export type OutcomeWithMonoBody<T> = {
+    status: OutcomeStatus.SUCCESS_MONO | OutcomeStatus.FAIL_DNE_MONO,
+    body: {
+        data: T,
+    },
+};
+
+export type OutcomeWithDuoBody<T> = {
+    status: OutcomeStatus.SUCCESS_DUO | OutcomeStatus.FAIL_DNE_DUO,
+    body: {
+        data1: T,
+        data2: T,
+    },
+};
+
+export type OptionValidationErrorOutcome<T> = {
+    status: OutcomeStatus.FAIL_VALIDATION,
+    body: {
+        constraint: Constraint<T>,
+        field: string,
+        value: T,
+    },
+};
+
+export type OutcomeWithDuoListBody<T, U> = {
+    status: OutcomeStatus.SUCCESS_NO_CHANGE,
+    body: {
+        data1: T[],
+        data2: U[],
+    },
+};
+
+export type Outcome<T, U = void> = OutcomeWithEmptyBody | OutcomeWithMonoBody<T> | OutcomeWithDuoBody<T> | OutcomeWithDuoListBody<T, U> | OptionValidationErrorOutcome<T>;
