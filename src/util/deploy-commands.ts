f6956c90844e968ea57020456db4cc59c8a62b3b
coreybriscoe <coreybriscoe@gmail.com>
2023-11-14 14:55:07 -0500

    refactor: Convert cmd deploy script to expect Rendezvous

--- src/util/deploy-commands.ts
+++ src/util/deploy-commands.ts
@@ -5,17 +5,17 @@ import path from 'path';
 import { pathToFileURL, fileURLToPath } from 'url';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { CustomCommand } from '../types/customCommand.js';
 import { RESTPostAPIApplicationCommandsJSONBody } from 'discord.js';
+import { RendezvousCommand } from '../commands/slashcommands/architecture/rendezvousCommand.js';
 
 const commands = new Array<RESTPostAPIApplicationCommandsJSONBody>();
 const commandsPath = pathToFileURL(path.join(process.cwd(), './src/commands'));
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+const commandFiles = fs.readdirSync(commandsPath, { encoding: 'utf8', recursive: false }).filter((file: string) => file.endsWith('.js') || file.endsWith('.ts'));
 
 for (const file of commandFiles) {
     const filePath = pathToFileURL(path.join(fileURLToPath(commandsPath), file)).toString();
-    const command = (await import(filePath)).default as CustomCommand;
-    commands.push(command.data.toJSON());
+    const command = (await import(filePath)).default as RendezvousCommand<unknown, unknown, unknown>;
+    commands.push(command.interfacer!.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN as string);
