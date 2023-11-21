83aabaa44e69403786d4eb4a163c667851cdf0f2
coreybriscoe <coreybriscoe@gmail.com>
2023-11-21 15:23:04 -0500

    refactor: Update command handler paths (#53)

--- src/util/commandHandler.ts
+++ src/util/commandHandler.ts
@@ -5,10 +5,16 @@ import { TournamentionClient } from '../types/client.js';
 
 export const prepareCommands = async (client: TournamentionClient) => {
     const commandsPath = pathToFileURL(path.join(process.cwd(), './src/commands'));
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts'));
+    let commandFiles = new Array<string>();
+    // Slash commands (also known as Chat Input commands)
+    const slashCommandsPath = pathToFileURL(path.join(fileURLToPath(commandsPath), './slashcommands'));
+    commandFiles = commandFiles.concat(fs.readdirSync(slashCommandsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts')));
+    // Message commands
+    const messageCommandsPath = pathToFileURL(path.join(fileURLToPath(commandsPath), './messagecommands'));
+    commandFiles = commandFiles.concat(fs.readdirSync(messageCommandsPath).filter(file => file.endsWith('.js') || file.endsWith('.ts')));
 
     for (const file of commandFiles) {
-        const filePath = pathToFileURL(path.join(fileURLToPath(commandsPath), file)).toString();
+        const filePath = pathToFileURL(path.join(fileURLToPath(slashCommandsPath), file)).toString();
         const command = (await import(filePath)).default;
         client.addCommands([{ name: command.interfacer.name, command: command }]);
     }
